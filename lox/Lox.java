4f1f72abe37c6769a0cf62730adb0ffee6c4cc02
Divyaank Tiwari <divyaankt21@gmail.com>
2022-07-17 00:49:32 +0530

    Fixed package import, also defined runFile() and runPrompt()

--- lox/Lox.java
+++ lox/Lox.java
@@ -1,4 +1,4 @@
-public package com.craftinginterpreters.lox;
+package com.craftinginterpreters.lox;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -12,11 +12,29 @@ public class Lox {
   public static void main(String[] args) throws IOException {
     if (args.length > 1) {
       System.out.println("Usage: jlox [script]");
-      System.exit(64); 
+      System.exit(64);
     } else if (args.length == 1) {
       runFile(args[0]);
     } else {
       runPrompt();
+    }
+  }
+
+  private static void runFile(String path) throws IOException {
+    byte[] bytes = Files.readAllBytes(Paths.get(path));
+    run(new String(bytes, Charset.defaultCharset()));
+  }
+
+  private static void runPrompt() throws IOException {
+    InputStreamReader inpput = new InputStreamReader(System.in);
+    BufferedReader reader = new BufferedReader(input);
+
+    for (;;) {
+      System.out.print(">");
+      String line = reader.readLine();
+      if (line == null)
+        break;
+      run(line);
     }
   }
 }
