7a3becd2c8cf2974300fad1e3906af167b771f54
Divyaank Tiwari <divyaankt21@gmail.com>
2022-07-17 01:10:04 +0530

    Basic error handling logic implemented

--- lox/Lox.java
+++ lox/Lox.java
@@ -9,6 +9,9 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class Lox {
+
+  static boolean hadError = false;
+
   public static void main(String[] args) throws IOException {
     if (args.length > 1) {
       System.out.println("Usage: jlox [script]");
@@ -23,6 +26,10 @@ public class Lox {
   private static void runFile(String path) throws IOException {
     byte[] bytes = Files.readAllBytes(Paths.get(path));
     run(new String(bytes, Charset.defaultCharset()));
+
+    // Indicates an error in exit code
+    if (hadError)
+      System.exit(65);
   }
 
   private static void runPrompt() throws IOException {
@@ -35,6 +42,7 @@ public class Lox {
       if (line == null)
         break;
       run(line);
+      hadError = false;
     }
   }
 
@@ -45,5 +53,14 @@ public class Lox {
     for (Token token : tokens) {
       System.out.println(token);
     }
+  }
+
+  static void error(int line, String message) {
+    report(line, "", message);
+  }
+
+  private static void report(int line, String where, String message) {
+    System.err.println("[line " + line + "] Error" + where + ": " + message);
+    hadError = true;
   }
 }
