5749c542ca6e21bb56cb27278ba6f5398b21a331
Divyaank Tiwari <divyaankt21@gmail.com>
2022-07-17 19:26:56 +0530

    Scanner extended to handle identifiers

--- lang/lox/Scanner.java
+++ lang/lox/Scanner.java
@@ -99,11 +99,20 @@ class Scanner {
             default:
                 if (isDigit(c)) {
                     number();
+                } else if (isAlpha(c)) {
+                    identifier();
                 } else {
                     Lox.error(line, "Unexpected character.");
                 }
                 break;
         }
+    }
+
+    private void identifier() {
+        while (isAlphaNumeric(peek()))
+            advance();
+
+        addToken(IDENTIFIER);
     }
 
     private void number() {
@@ -164,6 +173,16 @@ class Scanner {
         if (current + 1 >= source.length())
             return '\0';
         return source.charAt(current + 1);
+    }
+
+    private boolean isAlpha(char c) {
+        return (c >= 'a' && c <= 'z') ||
+                (c >= 'A' && c <= 'Z') ||
+                c == '_';
+    }
+
+    private boolean isAlphaNumeric(char c) {
+        return isAlpha(c) || isDigit(c);
     }
 
     private boolean isDigit(char c) {
