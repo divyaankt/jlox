791b93a04f479fb4806031cb922f6bd72e887ecc
Divyaank Tiwari <divyaankt21@gmail.com>
2022-07-17 17:00:14 +0530

    Defined base Scanner class

---
+++ lang/lox/Scanner.java
@@ -0,0 +1,17 @@
+package lang.lox;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static lang.lox.TokenType.*; 
+
+class Scanner {
+  private final String source;
+  private final List<Token> tokens = new ArrayList<>();
+
+  Scanner(String source) {
+    this.source = source;
+  }
+}
