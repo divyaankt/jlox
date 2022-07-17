7f2c235f55458d9377f96acbec99d4f0ea6359a8
Divyaank Tiwari <divyaankt21@gmail.com>
2022-07-17 17:09:13 +0530

    Defined high level logic of Scanner

--- lang/lox/Scanner.java
+++ lang/lox/Scanner.java
@@ -5,13 +5,31 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import static lang.lox.TokenType.*; 
+import static lang.lox.TokenType.*;
 
 class Scanner {
-  private final String source;
-  private final List<Token> tokens = new ArrayList<>();
+    private final String source;
+    private final List<Token> tokens = new ArrayList<>();
+    private int start = 0;
+    private int current = 0;
+    private int line = 1;
 
-  Scanner(String source) {
-    this.source = source;
-  }
+    Scanner(String source) {
+        this.source = source;
+    }
+
+    List<Token> scanTokens() {
+        while (!isAtEnd()) {
+            // At the beginning of the next lexeme
+            start = current;
+            scanToken();
+        }
+
+        tokens.add(new Token(EOF, "", null, line));
+        return tokens;
+    }
+
+    private boolean isAtEnd() {
+        return current >= source.length();
+    }
 }
